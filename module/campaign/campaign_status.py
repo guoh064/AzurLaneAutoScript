c8f571e364238e19deda6b61521d10624de681b0
guoh064 <50830808+guoh064@users.noreply.github.com>
2024-10-10 23:55:17 +0800

    tmp: [JP] Ocr Digits

--- module/campaign/campaign_status.py
+++ module/campaign/campaign_status.py
@@ -3,6 +3,7 @@ import re
 import cv2
 import numpy as np
 
+import module.config.server as server
 from module.base.timer import Timer
 from module.base.utils import color_similar, get_color
 from module.campaign.assets import OCR_COIN, OCR_EVENT_PT, OCR_OIL, OCR_OIL_CHECK
@@ -10,7 +11,10 @@ from module.logger import logger
 from module.ocr.ocr import Digit, Ocr
 from module.ui.ui import UI
 
-OCR_COIN = Digit(OCR_COIN, name='OCR_COIN', letter=(239, 239, 239), threshold=128)
+if server.server != 'jp':
+    OCR_COIN = Digit(OCR_COIN, name='OCR_COIN', letter=(239, 239, 239), threshold=128)
+else:
+    OCR_COIN = Digit(OCR_COIN, name='OCR_COIN', letter=(192, 192, 192), threshold=128)
 
 
 class PtOcr(Ocr):
@@ -84,7 +88,7 @@ class CampaignStatus(UI):
         color = get_color(self.device.image, OCR_OIL_CHECK.button)
         if color_similar(color, OCR_OIL_CHECK.color):
             # Original color
-            ocr = Digit(OCR_OIL, name='OCR_OIL', letter=(247, 247, 247), threshold=128)
+            ocr = Digit(OCR_OIL, name='OCR_OIL', letter=(192, 192, 192), threshold=128)
         elif color_similar(color, (59, 59, 64)):
             # With black overlay
             ocr = Digit(OCR_OIL, name='OCR_OIL', letter=(165, 165, 165), threshold=128)
--- module/ocr/ocr.py
+++ module/ocr/ocr.py
@@ -140,7 +140,7 @@ class Digit(Ocr):
     Method ocr() returns int, or a list of int.
     """
 
-    def __init__(self, buttons, lang='azur_lane', letter=(255, 255, 255), threshold=128, alphabet='0123456789IDSB',
+    def __init__(self, buttons, lang='azur_lane', letter=(255, 255, 255), threshold=128, alphabet='0123456789IDSBUVT',
                  name=None):
         super().__init__(buttons, lang=lang, letter=letter, threshold=threshold, alphabet=alphabet, name=name)
 
@@ -148,6 +148,7 @@ class Digit(Ocr):
         result = super().after_process(result)
         result = result.replace('I', '1').replace('D', '0').replace('S', '5')
         result = result.replace('B', '8')
+        result = result.replace('V', '0').replace('T', '1').replace('U', '0')
 
         prev = result
         result = int(result) if result else 0
@@ -163,7 +164,7 @@ class DigitYuv(Digit, OcrYuv):
 
 
 class DigitCounter(Ocr):
-    def __init__(self, buttons, lang='azur_lane', letter=(255, 255, 255), threshold=128, alphabet='0123456789/IDSB',
+    def __init__(self, buttons, lang='azur_lane', letter=(255, 255, 255), threshold=128, alphabet='0123456789/IDSBUVT',
                  name=None):
         super().__init__(buttons, lang=lang, letter=letter, threshold=threshold, alphabet=alphabet, name=name)
 
@@ -171,6 +172,7 @@ class DigitCounter(Ocr):
         result = super().after_process(result)
         result = result.replace('I', '1').replace('D', '0').replace('S', '5')
         result = result.replace('B', '8')
+        result = result.replace('V', '0').replace('T', '1').replace('U', '0')
         return result
 
     def ocr(self, image, direct_ocr=False):
